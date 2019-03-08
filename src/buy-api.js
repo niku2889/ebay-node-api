90ca53d3b70e16a1fa101992458ab38eab5b8b22
Nikunj Sathwara <nikunjsathwara039@gmail.com>
2019-03-09 00:11:15 +0530

    Update buy-api.js

--- src/buy-api.js
+++ src/buy-api.js
@@ -1,6 +1,8 @@
 
 
-const { makeRequest, makeRequestUSA, makeRequestGermany } = require('./request');
+const { makeRequest, makeRequestUSA, makeRequestGermany, makeRequestAT, makeRequestAU, makeRequestBE, makeRequestCA,
+    makeRequestCH, makeRequestES, makeRequestFR, makeRequestIE,
+    makeRequestIT, makeRequestNL } = require('./request');
 
 const getItem = function (itemId) {
     if (!itemId) throw new Error("Item Id is required");
@@ -105,11 +107,232 @@ const searchItemsGermany = function (searchConfig) {
     });
 };
 
+const searchItemsAT = function (searchConfig) {
+    if (!searchConfig) throw new Error("Error --> Missing or invalid input parameter to search");
+    if (!searchConfig.keyword && !searchConfig.categoryId && !searchConfig.gtin) throw new Error("Error --> Keyword or category id is required in query param");
+    if (!this.options.access_token) throw new Error("Error -->Missing Access token, Generate access token");
+    const auth = "Bearer " + this.options.access_token;
+    let queryParam = searchConfig.keyword ? "q=" + searchConfig.keyword : "";
+    queryParam = queryParam + (searchConfig.gtin ? "&gtin=" + searchConfig.gtin : '');
+    queryParam = queryParam + (searchConfig.categoryId ? "&category_ids=" + searchConfig.categoryId : '');
+    queryParam = queryParam + (searchConfig.limit ? "&limit=" + searchConfig.limit : "");
+    queryParam = queryParam + (searchConfig.sort ? "&sort=" + searchConfig.sort : "");
+    if (searchConfig.fieldgroups != undefined) queryParam = queryParam + "&fieldgroups=" + searchConfig.fieldgroups;
+    if (searchConfig.filter != undefined) queryParam = queryParam + "&filter=" + searchConfig.filter;
+    return new Promise((resolve, reject) => {
+        makeRequestAT(this.options.baseUrl, `/buy/browse/v1/item_summary/search?${queryParam}`, 'GET', this.options.body, auth).then((result) => {
+            resolve(result);
+        }).then((error) => {
+            reject(error);
+        });
+    });
+};
+
+const searchItemsAU = function (searchConfig) {
+    if (!searchConfig) throw new Error("Error --> Missing or invalid input parameter to search");
+    if (!searchConfig.keyword && !searchConfig.categoryId && !searchConfig.gtin) throw new Error("Error --> Keyword or category id is required in query param");
+    if (!this.options.access_token) throw new Error("Error -->Missing Access token, Generate access token");
+    const auth = "Bearer " + this.options.access_token;
+    let queryParam = searchConfig.keyword ? "q=" + searchConfig.keyword : "";
+    queryParam = queryParam + (searchConfig.gtin ? "&gtin=" + searchConfig.gtin : '');
+    queryParam = queryParam + (searchConfig.categoryId ? "&category_ids=" + searchConfig.categoryId : '');
+    queryParam = queryParam + (searchConfig.limit ? "&limit=" + searchConfig.limit : "");
+    queryParam = queryParam + (searchConfig.sort ? "&sort=" + searchConfig.sort : "");
+    if (searchConfig.fieldgroups != undefined) queryParam = queryParam + "&fieldgroups=" + searchConfig.fieldgroups;
+    if (searchConfig.filter != undefined) queryParam = queryParam + "&filter=" + searchConfig.filter;
+    return new Promise((resolve, reject) => {
+        makeRequestAU(this.options.baseUrl, `/buy/browse/v1/item_summary/search?${queryParam}`, 'GET', this.options.body, auth).then((result) => {
+            resolve(result);
+        }).then((error) => {
+            reject(error);
+        });
+    });
+};
+
+const searchItemsBE = function (searchConfig) {
+    if (!searchConfig) throw new Error("Error --> Missing or invalid input parameter to search");
+    if (!searchConfig.keyword && !searchConfig.categoryId && !searchConfig.gtin) throw new Error("Error --> Keyword or category id is required in query param");
+    if (!this.options.access_token) throw new Error("Error -->Missing Access token, Generate access token");
+    const auth = "Bearer " + this.options.access_token;
+    let queryParam = searchConfig.keyword ? "q=" + searchConfig.keyword : "";
+    queryParam = queryParam + (searchConfig.gtin ? "&gtin=" + searchConfig.gtin : '');
+    queryParam = queryParam + (searchConfig.categoryId ? "&category_ids=" + searchConfig.categoryId : '');
+    queryParam = queryParam + (searchConfig.limit ? "&limit=" + searchConfig.limit : "");
+    queryParam = queryParam + (searchConfig.sort ? "&sort=" + searchConfig.sort : "");
+    if (searchConfig.fieldgroups != undefined) queryParam = queryParam + "&fieldgroups=" + searchConfig.fieldgroups;
+    if (searchConfig.filter != undefined) queryParam = queryParam + "&filter=" + searchConfig.filter;
+    return new Promise((resolve, reject) => {
+        makeRequestBE(this.options.baseUrl, `/buy/browse/v1/item_summary/search?${queryParam}`, 'GET', this.options.body, auth).then((result) => {
+            resolve(result);
+        }).then((error) => {
+            reject(error);
+        });
+    });
+};
+
+const searchItemsCA = function (searchConfig) {
+    if (!searchConfig) throw new Error("Error --> Missing or invalid input parameter to search");
+    if (!searchConfig.keyword && !searchConfig.categoryId && !searchConfig.gtin) throw new Error("Error --> Keyword or category id is required in query param");
+    if (!this.options.access_token) throw new Error("Error -->Missing Access token, Generate access token");
+    const auth = "Bearer " + this.options.access_token;
+    let queryParam = searchConfig.keyword ? "q=" + searchConfig.keyword : "";
+    queryParam = queryParam + (searchConfig.gtin ? "&gtin=" + searchConfig.gtin : '');
+    queryParam = queryParam + (searchConfig.categoryId ? "&category_ids=" + searchConfig.categoryId : '');
+    queryParam = queryParam + (searchConfig.limit ? "&limit=" + searchConfig.limit : "");
+    queryParam = queryParam + (searchConfig.sort ? "&sort=" + searchConfig.sort : "");
+    if (searchConfig.fieldgroups != undefined) queryParam = queryParam + "&fieldgroups=" + searchConfig.fieldgroups;
+    if (searchConfig.filter != undefined) queryParam = queryParam + "&filter=" + searchConfig.filter;
+    return new Promise((resolve, reject) => {
+        makeRequestCA(this.options.baseUrl, `/buy/browse/v1/item_summary/search?${queryParam}`, 'GET', this.options.body, auth).then((result) => {
+            resolve(result);
+        }).then((error) => {
+            reject(error);
+        });
+    });
+};
+
+const searchItemsCH = function (searchConfig) {
+    if (!searchConfig) throw new Error("Error --> Missing or invalid input parameter to search");
+    if (!searchConfig.keyword && !searchConfig.categoryId && !searchConfig.gtin) throw new Error("Error --> Keyword or category id is required in query param");
+    if (!this.options.access_token) throw new Error("Error -->Missing Access token, Generate access token");
+    const auth = "Bearer " + this.options.access_token;
+    let queryParam = searchConfig.keyword ? "q=" + searchConfig.keyword : "";
+    queryParam = queryParam + (searchConfig.gtin ? "&gtin=" + searchConfig.gtin : '');
+    queryParam = queryParam + (searchConfig.categoryId ? "&category_ids=" + searchConfig.categoryId : '');
+    queryParam = queryParam + (searchConfig.limit ? "&limit=" + searchConfig.limit : "");
+    queryParam = queryParam + (searchConfig.sort ? "&sort=" + searchConfig.sort : "");
+    if (searchConfig.fieldgroups != undefined) queryParam = queryParam + "&fieldgroups=" + searchConfig.fieldgroups;
+    if (searchConfig.filter != undefined) queryParam = queryParam + "&filter=" + searchConfig.filter;
+    return new Promise((resolve, reject) => {
+        makeRequestCH(this.options.baseUrl, `/buy/browse/v1/item_summary/search?${queryParam}`, 'GET', this.options.body, auth).then((result) => {
+            resolve(result);
+        }).then((error) => {
+            reject(error);
+        });
+    });
+};
+
+const searchItemsES = function (searchConfig) {
+    if (!searchConfig) throw new Error("Error --> Missing or invalid input parameter to search");
+    if (!searchConfig.keyword && !searchConfig.categoryId && !searchConfig.gtin) throw new Error("Error --> Keyword or category id is required in query param");
+    if (!this.options.access_token) throw new Error("Error -->Missing Access token, Generate access token");
+    const auth = "Bearer " + this.options.access_token;
+    let queryParam = searchConfig.keyword ? "q=" + searchConfig.keyword : "";
+    queryParam = queryParam + (searchConfig.gtin ? "&gtin=" + searchConfig.gtin : '');
+    queryParam = queryParam + (searchConfig.categoryId ? "&category_ids=" + searchConfig.categoryId : '');
+    queryParam = queryParam + (searchConfig.limit ? "&limit=" + searchConfig.limit : "");
+    queryParam = queryParam + (searchConfig.sort ? "&sort=" + searchConfig.sort : "");
+    if (searchConfig.fieldgroups != undefined) queryParam = queryParam + "&fieldgroups=" + searchConfig.fieldgroups;
+    if (searchConfig.filter != undefined) queryParam = queryParam + "&filter=" + searchConfig.filter;
+    return new Promise((resolve, reject) => {
+        makeRequestES(this.options.baseUrl, `/buy/browse/v1/item_summary/search?${queryParam}`, 'GET', this.options.body, auth).then((result) => {
+            resolve(result);
+        }).then((error) => {
+            reject(error);
+        });
+    });
+};
+
+const searchItemsFR = function (searchConfig) {
+    if (!searchConfig) throw new Error("Error --> Missing or invalid input parameter to search");
+    if (!searchConfig.keyword && !searchConfig.categoryId && !searchConfig.gtin) throw new Error("Error --> Keyword or category id is required in query param");
+    if (!this.options.access_token) throw new Error("Error -->Missing Access token, Generate access token");
+    const auth = "Bearer " + this.options.access_token;
+    let queryParam = searchConfig.keyword ? "q=" + searchConfig.keyword : "";
+    queryParam = queryParam + (searchConfig.gtin ? "&gtin=" + searchConfig.gtin : '');
+    queryParam = queryParam + (searchConfig.categoryId ? "&category_ids=" + searchConfig.categoryId : '');
+    queryParam = queryParam + (searchConfig.limit ? "&limit=" + searchConfig.limit : "");
+    queryParam = queryParam + (searchConfig.sort ? "&sort=" + searchConfig.sort : "");
+    if (searchConfig.fieldgroups != undefined) queryParam = queryParam + "&fieldgroups=" + searchConfig.fieldgroups;
+    if (searchConfig.filter != undefined) queryParam = queryParam + "&filter=" + searchConfig.filter;
+    return new Promise((resolve, reject) => {
+        makeRequestFR(this.options.baseUrl, `/buy/browse/v1/item_summary/search?${queryParam}`, 'GET', this.options.body, auth).then((result) => {
+            resolve(result);
+        }).then((error) => {
+            reject(error);
+        });
+    });
+};
+
+const searchItemsIE = function (searchConfig) {
+    if (!searchConfig) throw new Error("Error --> Missing or invalid input parameter to search");
+    if (!searchConfig.keyword && !searchConfig.categoryId && !searchConfig.gtin) throw new Error("Error --> Keyword or category id is required in query param");
+    if (!this.options.access_token) throw new Error("Error -->Missing Access token, Generate access token");
+    const auth = "Bearer " + this.options.access_token;
+    let queryParam = searchConfig.keyword ? "q=" + searchConfig.keyword : "";
+    queryParam = queryParam + (searchConfig.gtin ? "&gtin=" + searchConfig.gtin : '');
+    queryParam = queryParam + (searchConfig.categoryId ? "&category_ids=" + searchConfig.categoryId : '');
+    queryParam = queryParam + (searchConfig.limit ? "&limit=" + searchConfig.limit : "");
+    queryParam = queryParam + (searchConfig.sort ? "&sort=" + searchConfig.sort : "");
+    if (searchConfig.fieldgroups != undefined) queryParam = queryParam + "&fieldgroups=" + searchConfig.fieldgroups;
+    if (searchConfig.filter != undefined) queryParam = queryParam + "&filter=" + searchConfig.filter;
+    return new Promise((resolve, reject) => {
+        makeRequestIE(this.options.baseUrl, `/buy/browse/v1/item_summary/search?${queryParam}`, 'GET', this.options.body, auth).then((result) => {
+            resolve(result);
+        }).then((error) => {
+            reject(error);
+        });
+    });
+};
+
+const searchItemsIT = function (searchConfig) {
+    if (!searchConfig) throw new Error("Error --> Missing or invalid input parameter to search");
+    if (!searchConfig.keyword && !searchConfig.categoryId && !searchConfig.gtin) throw new Error("Error --> Keyword or category id is required in query param");
+    if (!this.options.access_token) throw new Error("Error -->Missing Access token, Generate access token");
+    const auth = "Bearer " + this.options.access_token;
+    let queryParam = searchConfig.keyword ? "q=" + searchConfig.keyword : "";
+    queryParam = queryParam + (searchConfig.gtin ? "&gtin=" + searchConfig.gtin : '');
+    queryParam = queryParam + (searchConfig.categoryId ? "&category_ids=" + searchConfig.categoryId : '');
+    queryParam = queryParam + (searchConfig.limit ? "&limit=" + searchConfig.limit : "");
+    queryParam = queryParam + (searchConfig.sort ? "&sort=" + searchConfig.sort : "");
+    if (searchConfig.fieldgroups != undefined) queryParam = queryParam + "&fieldgroups=" + searchConfig.fieldgroups;
+    if (searchConfig.filter != undefined) queryParam = queryParam + "&filter=" + searchConfig.filter;
+    return new Promise((resolve, reject) => {
+        makeRequestIT(this.options.baseUrl, `/buy/browse/v1/item_summary/search?${queryParam}`, 'GET', this.options.body, auth).then((result) => {
+            resolve(result);
+        }).then((error) => {
+            reject(error);
+        });
+    });
+};
+
+const searchItemsNL = function (searchConfig) {
+    if (!searchConfig) throw new Error("Error --> Missing or invalid input parameter to search");
+    if (!searchConfig.keyword && !searchConfig.categoryId && !searchConfig.gtin) throw new Error("Error --> Keyword or category id is required in query param");
+    if (!this.options.access_token) throw new Error("Error -->Missing Access token, Generate access token");
+    const auth = "Bearer " + this.options.access_token;
+    let queryParam = searchConfig.keyword ? "q=" + searchConfig.keyword : "";
+    queryParam = queryParam + (searchConfig.gtin ? "&gtin=" + searchConfig.gtin : '');
+    queryParam = queryParam + (searchConfig.categoryId ? "&category_ids=" + searchConfig.categoryId : '');
+    queryParam = queryParam + (searchConfig.limit ? "&limit=" + searchConfig.limit : "");
+    queryParam = queryParam + (searchConfig.sort ? "&sort=" + searchConfig.sort : "");
+    if (searchConfig.fieldgroups != undefined) queryParam = queryParam + "&fieldgroups=" + searchConfig.fieldgroups;
+    if (searchConfig.filter != undefined) queryParam = queryParam + "&filter=" + searchConfig.filter;
+    return new Promise((resolve, reject) => {
+        makeRequestNL(this.options.baseUrl, `/buy/browse/v1/item_summary/search?${queryParam}`, 'GET', this.options.body, auth).then((result) => {
+            resolve(result);
+        }).then((error) => {
+            reject(error);
+        });
+    });
+};
+
+
 module.exports = {
     getItem,
     getItemByLegacyId,
     getItemByItemGroup,
     searchItems,
     searchItemsUSA,
-    searchItemsGermany
+    searchItemsGermany,
+    searchItemsAT,
+    searchItemsAU,
+    searchItemsBE,
+    searchItemsCA,
+    searchItemsCH,
+    searchItemsES,
+    searchItemsFR,
+    searchItemsIE,
+    searchItemsIT,
+    searchItemsNL
 }
