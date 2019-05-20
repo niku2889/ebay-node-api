806e501ae4f08db7ce8c57f0b2d429994f24114a
Nikunj Sathwara <nikunjsathwara039@gmail.com>
2019-05-20 13:50:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ function Ebay(options) {
         options.baseSvcUrl = BASE_SANDBX_SVC_URL;
     }
     this.options = options;
-    this.options.globalID = options.countryCode || "EBAY-US";
+    this.options.globalID = options.countryCode || "EBAY-DE";
 }
 
 Ebay.prototype = {
